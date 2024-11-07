9559f6232ea9d25d6cc49bdff5c6f984fd7a49e7
mabem95 <jangdo95@gmail.com>
2024-11-07 15:56:07 +0900

    refact: #209 제안 검색조건 querydsl 설정

--- src/main/java/beyond/samdasoo/lead/dto/LeadSearchCond.java
+++ src/main/java/beyond/samdasoo/lead/dto/LeadSearchCond.java
@@ -3,12 +3,10 @@ package beyond.samdasoo.lead.dto;
 import beyond.samdasoo.common.dto.SearchCond;
 import beyond.samdasoo.lead.Entity.LeadStatus;
 import lombok.Getter;
-import lombok.ToString;
 
 import java.time.LocalDate;
 
 @Getter
-@ToString
 public class LeadSearchCond extends SearchCond {
     private final LeadStatus status;
     private final Long process;
@@ -17,7 +15,7 @@ public class LeadSearchCond extends SearchCond {
 
     public LeadSearchCond(LocalDate searchDate, LocalDate startDate, LocalDate endDate, Long deptNo, Long userNo,
                           LeadStatus status, Long process, Long subProcess, String name) {
-        super(searchDate, startDate, endDate, deptNo, userNo); // 부모 클래스의 생성자를 호출합니다.
+        super(searchDate, startDate, endDate, deptNo, userNo);
         this.status = status;
         this.process = process;
         this.subProcess = subProcess;
